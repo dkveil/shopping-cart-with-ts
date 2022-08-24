3500bf4dc8e927f5b9006b460174729a1deb526a
Damian Kądziela <dkkquality@gmail.com>
2022-08-24 18:27:17 +0200

    fetching data to store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,15 +1,42 @@
-import storeitems from "../data/items.json";
+import React from "react";
 import { Wrapper, ContentWrapper } from "../containers/sectionStyles";
 import ProductCart from "../components/ProductCart";
 
+type storeItemProps = {
+    id: number,
+    name: string;
+    price: number;
+    image: string;
+}
+
 const Store = () => {
+
+    const [storeItems, setStoreItems] = React.useState<storeItemProps[]>([])
+
+    React.useEffect(() => {
+        fetch("./data/items.json", {
+            method: "GET",
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res;
+                }
+            })
+            .then((res) => res?.json())
+            .then((data) => {
+                setStoreItems(data);
+            })
+            .catch((error) => console.log(error));
+
+    }, [])
+
     return (
         <Wrapper>
             <h1>Store</h1>
             <ContentWrapper
             justifycontent="space-between"
             >
-                {storeitems.map(item => (
+                {storeItems && storeItems.map(item => (
                     <ProductCart
                         key={item.id}
                         id={item.id}
